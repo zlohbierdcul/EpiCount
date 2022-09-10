c08f979b217d8c2f55e3a7f87d2627e11f982c43
zlohbierdcul <82916072+zlohbierdcul@users.noreply.github.com>
2022-09-10 21:44:51 +0200

    added Filler-Feature

--- counter_sender.py
+++ counter_sender.py
@@ -2,6 +2,7 @@ import discord
 import json
 from message_sender import send_message
 from chat_clearer import messageid
+from filler_manager import check_if_filler
 
 
 JSON_DATA_PATH = "data/data.json"
@@ -21,5 +22,13 @@ async def send_counter(key, channel):
 
     if (key != 'onepiece'):
         EMOJI_ARRAY.pop(3)
-    new_message = await send_message(channel=channel, title=data[key]["name"], description=f'Aktuelle Folge: Staffel {data[key]["season"]} Folge {data[key]["episode"]}', color=discord.Color.from_rgb(0,255,0), reactions=EMOJI_ARRAY)
+
+    if (key == 'onepiece'):
+        filler_array = check_if_filler(data[key]['episode'])
+        is_filler = filler_array[0]
+        last_filler = filler_array[1]
+        epi_till_last = filler_array[2]
+        filler_message = f'Filler Folge! Ende: {last_filler}, noch {epi_till_last} Folgen!'
+        no_filler_message = 'Keine Filler Folge!'
+    new_message = await send_message(channel=channel, title=data[key]["name"], description=f'Aktuelle Folge: Staffel {data[key]["season"]} Folge {data[key]["episode"]} \n {filler_message if is_filler else no_filler_message}', color=discord.Color.from_rgb(0,255,0), reactions=EMOJI_ARRAY)
     messageid[key] = new_message.id
---
+++ filler_manager.py
@@ -0,0 +1,21 @@
+
+filler_episodes = [[54,60],[98,99],[102],[131,143],[196,206],[220,226],[279,283],[291,292],[303],[317,319],[326,336],[382,384],[406,407],[426,429],[457,458],[492],[542],[575,578],[590],[626,627],[747,750],[780,782],[895,896],[907],[1029,1030]]
+
+def check_if_filler(current_episode):
+    is_filler = False
+    last_filler = 0
+    episodes_till_last_filler = 0
+    for x in filler_episodes:
+        if len(x) > 1:
+            if (current_episode >= x[0] and current_episode <= x[1]):
+                is_filler = True
+                last_filler = x[1]
+                episodes_till_last_filler = last_filler - current_episode
+        else:
+            if x[0] == current_episode:
+                is_filler = True
+                last_filler = x[0]
+                episodes_till_last_filler = last_filler - current_episode
+    return [is_filler, last_filler, episodes_till_last_filler]
+        
+print(check_if_filler(143))
