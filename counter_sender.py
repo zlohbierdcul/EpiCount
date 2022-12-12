a837d1cd028c09dcfa46f88116c7c44509cd59ca
zlohbierdcul <82916072+zlohbierdcul@users.noreply.github.com>
2022-12-13 00:43:29 +0100

    bug fix

--- counter_sender.py
+++ counter_sender.py
@@ -30,7 +30,7 @@ async def send_counter(key, channel):
         last_filler = filler_array[1]
         epi_till_last = filler_array[2]
         filler_message = f'Filler Folge! Ende: {last_filler}, noch {epi_till_last} Folgen!'
-        no_filler_message = 'Keine Filler Folge!'
+    no_filler_message = 'Keine Filler Folge!'
     new_message = await send_message(channel=channel, title=data[key]["name"], description=f'Aktuelle Folge: Staffel {data[key]["season"]} Folge {data[key]["episode"]} \n {filler_message if is_filler else no_filler_message}', color=discord.Color.from_rgb(0,255,0), reactions=EMOJI_ARRAY)
     messageid[key] = new_message.id
     
